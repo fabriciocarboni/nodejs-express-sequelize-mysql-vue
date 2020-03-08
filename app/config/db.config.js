71c2e5725985a904c889807cc4a339e919d3cc3a
Fabricio <carboni@gmail.com>
2020-03-08 00:32:30 -0500

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ const config = require('dotenv').config(); // include .env file
 
 module.exports = {
   HOST: process.env.MYSQL_HOST,
-  USER: process.env.DEV_DB_USER,
+  USER: process.env.MYSQL_USER,
   PORT: process.env.MYSQL_PORT,
   PASSWORD: process.env.MYSQL_PASS,
   DB: process.env.MYSQL_DB,
